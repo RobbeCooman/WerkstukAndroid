221ffad903a441a61005f8643413d7b575720b99
Cooman Robbe <robbe.cooman@student.ehb.be>
2020-05-26 15:45:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/werkstukandroid/AddKlachtActivity.java
@@ -0,0 +1,17 @@
+package com.example.werkstukandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddKlachtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_klacht);
+    }
+
+
+
+}
